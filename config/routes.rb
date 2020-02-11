cee875f68f2591f8df0ec38e605b36720879ead6
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-11 12:33:56 +0900

    routingの設定をする。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'income' => 'incomes#index'
+  get 'variablecosts' => 'variable_costs#index'
+  get 'fixedcosts' => 'fixed_costs#index'
 end
