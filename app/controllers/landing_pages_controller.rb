c535bbb18f1cd7d56792272e26e06f316f9eaaf3
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-14 16:40:34 +0900

    UIがだいぶ綺麗になった気がする。

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,9 +1,19 @@
 class LandingPagesController < ApplicationController
   def index
+    @income = Income.all
+    @variable_cost = VariableCost.all
+    @fixed_cost = FixedCost.all
+
     gon.income_sum = Income.sum(:money) / 10000
     gon.variable_cost_sum = VariableCost.sum(:money) / 10000
     gon.fixed_cost_sum = FixedCost.sum(:money) / 10000
+
+    # 現在の経済状況を計算する？
   end
 
   def show; end
+
+  def setting; end
+
+  def report; end
 end
--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -1,4 +1,2 @@
 class LandingPage < ApplicationRecord
-  has_many :income
-  accepts_nested_attributes_for :income
 end
