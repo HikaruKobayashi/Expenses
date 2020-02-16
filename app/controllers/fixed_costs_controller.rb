f106d8366b99056e539536435e21d9d2e8fb66f7
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-16 18:42:43 +0900

    集計を年間、月間、週間で選択して見れるようにする。

--- app/controllers/fixed_costs_controller.rb
+++ app/controllers/fixed_costs_controller.rb
@@ -61,6 +61,10 @@ class FixedCostsController < ApplicationController
     @fixedCost = FixedCost.all
      # 総固定費
     gon.money_sum = FixedCost.sum(:money)
+     # 年毎の総固定費 単位1万円 TO DO DRY原則に反してる感がすごい
+     gon.money_year_1 = FixedCost.where(created_at: "2020-01-01".in_time_zone.all_year).sum(:money) / 10000
+     gon.money_year_2 = FixedCost.where(created_at: "2019-01-01".in_time_zone.all_year).sum(:money) / 10000
+     gon.money_year_3 = FixedCost.where(created_at: "2018-01-01".in_time_zone.all_year).sum(:money) / 10000
     # 月毎の総固定費 単位1万 TO DO DRY原則に反してる感がすごい
     gon.money_month_1 = FixedCost.where(created_at: "2020-01-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_2 = FixedCost.where(created_at: "2020-02-01".in_time_zone.all_month).sum(:money) / 10000
@@ -74,8 +78,18 @@ class FixedCostsController < ApplicationController
     gon.money_month_10 = FixedCost.where(created_at: "2020-10-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_11 = FixedCost.where(created_at: "2020-11-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_12 = FixedCost.where(created_at: "2020-12-01".in_time_zone.all_month).sum(:money) / 10000
-    # 1月から12月
+    # 週間レポート 単位1万円 TO DO DRY原則に反してる感がすごい
+    gon.money_7 = FixedCost.where(created_at: 6.day.ago.all_day).sum(:money) / 10000
+    gon.money_6 = FixedCost.where(created_at: 5.day.ago.all_day).sum(:money) / 10000
+    gon.money_5 = FixedCost.where(created_at: 4.day.ago.all_day).sum(:money) / 10000
+    gon.money_4 = FixedCost.where(created_at: 3.day.ago.all_day).sum(:money) / 10000
+    gon.money_3 = FixedCost.where(created_at: 2.day.ago.all_day).sum(:money) / 10000
+    gon.money_2 = FixedCost.where(created_at: 1.day.ago.all_day).sum(:money) / 10000
+    gon.money_1 = FixedCost.where(created_at: Time.zone.now.all_day).sum(:money) / 10000
+    # ラベル
+    gon.year = ["2018年", "2019年", "2020年"]
     gon.month = ["1月", "2月", "3月", "4月", "5月", "6月","7月", "8月", "9月", "10月", "11月", "12月"]
+    gon.week = ["7日前", "6日前", "5日前", "4日前", "3日前", "昨日", "今日"]
   end
 
   private
--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -61,7 +61,11 @@ class IncomesController < ApplicationController
     @income = Income.all
     # 総収入
     gon.money_sum = Income.sum(:money)
-    # 月毎の総収入 単位1万 TO DO DRY原則に反してる感がすごい
+    # 年毎の総収入 単位1万円 TO DO DRY原則に反してる感がすごい
+    gon.money_year_1 = Income.where(created_at: "2020-01-01".in_time_zone.all_year).sum(:money) / 10000
+    gon.money_year_2 = Income.where(created_at: "2019-01-01".in_time_zone.all_year).sum(:money) / 10000
+    gon.money_year_3 = Income.where(created_at: "2018-01-01".in_time_zone.all_year).sum(:money) / 10000
+    # 月毎の総収入 単位1万円 TO DO DRY原則に反してる感がすごい
     gon.money_month_1 = Income.where(created_at: "2020-01-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_2 = Income.where(created_at: "2020-02-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_3 = Income.where(created_at: "2020-03-01".in_time_zone.all_month).sum(:money) / 10000
@@ -74,8 +78,18 @@ class IncomesController < ApplicationController
     gon.money_month_10 = Income.where(created_at: "2020-10-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_11 = Income.where(created_at: "2020-11-01".in_time_zone.all_month).sum(:money) / 10000
     gon.money_month_12 = Income.where(created_at: "2020-12-01".in_time_zone.all_month).sum(:money) / 10000
-    # 1月から12月
+    # 週間レポート 単位1万円 TO DO DRY原則に反してる感がすごい
+    gon.money_7 = Income.where(created_at: 6.day.ago.all_day).sum(:money) / 10000
+    gon.money_6 = Income.where(created_at: 5.day.ago.all_day).sum(:money) / 10000
+    gon.money_5 = Income.where(created_at: 4.day.ago.all_day).sum(:money) / 10000
+    gon.money_4 = Income.where(created_at: 3.day.ago.all_day).sum(:money) / 10000
+    gon.money_3 = Income.where(created_at: 2.day.ago.all_day).sum(:money) / 10000
+    gon.money_2 = Income.where(created_at: 1.day.ago.all_day).sum(:money) / 10000
+    gon.money_1 = Income.where(created_at: Time.zone.now.all_day).sum(:money) / 10000
+    # ラベル
+    gon.year = ["2018年", "2019年", "2020年"]
     gon.month = ["1月", "2月", "3月", "4月", "5月", "6月","7月", "8月", "9月", "10月", "11月", "12月"]
+    gon.week = ["7日前", "6日前", "5日前", "4日前", "3日前", "昨日", "今日"]
   end
 
   private
