c7b82d1b91d72bc48b05b608ab2f319ce39c540b
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-16 17:38:50 +0900

    routingの設定を追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root to: 'landing_pages#index'
   resources :landing_pages
-  get "/setting" => "landing_pages#setting"
   get "/report" => "landing_pages#report"
 
   resources :incomes
+  get "/incomes_report" => "incomes#report"
   resources :variable_costs
+  get "/variable_costs_report" => "variable_costs#report"
   resources :fixed_costs
+  get "/fixed_costs_report" => "fixed_costs#report"
 end
