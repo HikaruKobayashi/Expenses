3ab048fc5e7d1b76f61a223c1f2bf0c21de03955
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-16 19:14:40 +0900

    お問い合わせ機能作成に向けて

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_023451) do
+ActiveRecord::Schema.define(version: 2020_02_16_100853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_02_15_023451) do
     t.datetime "updated_at", null: false
     t.integer "money"
     t.string "label"
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "landing_pages", force: :cascade do |t|
