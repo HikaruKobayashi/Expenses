d89648543e78e14ea233f332bbb227ef81580f34
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-17 18:41:51 +0900

    routes.rbの書き方を変更。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/report" => "landing_pages#report"
 
   resources :incomes
+  get "/incomes/new" => "incomes#new"
   get "/incomes_report" => "incomes#report"
   resources :variable_costs
   get "/variable_costs_report" => "variable_costs#report"
@@ -13,9 +14,9 @@ Rails.application.routes.draw do
   resources :save
   get "/saves_report" => "save#report"
 
-  get 'inquiry' => 'inquiry#index'
-  post 'inquiry/confirm' => 'inquiry#confirm'
-  post 'inquiry/thanks' => 'inquiry#thanks'
+  get '/inquiry' => 'inquiry#index'
+  post '/inquiry/confirm' => 'inquiry#confirm'
+  post '/inquiry/thanks' => 'inquiry#thanks'
 
   # ルーティングエラー時の処理
   get "*path" => 'landing_pages#index'
